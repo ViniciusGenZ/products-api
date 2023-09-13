6e09fe3e4a4a4ed034578b5db4d65ffd772509e1
viniciusjacik <viniciusferreira@genz.com.py>
2023-09-13 15:41:52 -0400

    FEAT We do not talk about this

--- src/services/products/SGetAllProducts.ts
+++ src/services/products/SGetAllProducts.ts
@@ -8,7 +8,35 @@ interface IResponse {
 }
 
 async function SGetAllProducts(input: FilterProperties): Promise<IResponse> {
-	return RGetAllProducts(input);
+	const { count, products } = await RGetAllProducts(input);
+	return {
+		count,
+		products: products.map((item) => {
+			if (item.productHasImages.length === 0) {
+				return {
+					...item,
+					productHasImages: [
+						{
+							status_active: true,
+							created_by: 1,
+							updated_by: 1,
+							deleted_by: null,
+							created_at: '2023-09-12T19:42:16.065Z',
+							updated_at: '2023-09-12T19:42:16.065Z',
+							deleted_at: null,
+							id_product_image: 0,
+							description_br: null,
+							description_en: null,
+							description_py: null,
+							url_image: 'misc/gz/noDisponible.png',
+							base_64: null,
+						},
+					],
+				} as unknown as Product;
+			}
+			return item;
+		}),
+	};
 }
 
 export default SGetAllProducts;
